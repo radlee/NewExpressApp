5ee497e74c642d8f55255b78f311e40d3bec3e2a
radlee <lolito@projectcodex.co>
2017-03-03 12:58:36 +0200

    BodyParser and ExpressHandlebars installed and configured

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+
+
 var app = express();
 
+
+//Configure bodyParser
+app.use(bodyParser.json()); //Support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); //Support encoded bodies
+
+
+//Configure ExpressJs to display content from static files
+app.use(express.static('public'));
+
+//Configure Handlebars as a view engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+//Simple Routing
 app.get('/', function(req, res){
   res.send('Hello World')
-})
+});
+
+//Respond to a Post Request on the root route (/), the apps home page
+app.post('/', function(req, res){
+  res.send('Got a POST Request')
+});
+
+//Respond to a PUT request to the /user route
+app.put('/user', function(req, res){
+  res.send('Got a PUT request at /user')
+});
+
+//Respond to a DELETE request to the /user route
+app.delete('/user', function(req, res){
+  res.send('Got a DELETE request at /user')
+});
+
 
 var port = 3000;
 app.listen(port, function(){
