bdd17778a50b1489c5e84cb965dd352c76ed5058
radlee <lolito@projectcodex.co>
2017-03-03 21:14:19 +0200

    Custom Style Added

--- app.js
+++ app.js
@@ -1,46 +1,45 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
+'use strict';
 
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+// var mysql = require('mysql');
+var previous = require('./routes/previous');
 
 var app = express();
 
+//My SQL ------------------------------------
+// var connection = mysql.createConnection({
+//   host : 'localhost',
+//   user : 'lee',
+//   password : 'password',
+//   database : 'my_db'
+// });
+//
+// connection.connect();
+// END MYSQL ----------------------------------
 
-//Configure bodyParser
+//Configure BodyParser
 app.use(bodyParser.json()); //Support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); //Support encoded bodies
 
-
 //Configure ExpressJs to display content from static files
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 //Configure Handlebars as a view engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
-//Simple Routing
-app.get('/', function(req, res){
-  res.send('Hello World')
+//Simple Routing 1.1
+app.get('/', function(req, res, next){
+  res.render('home', {layout: false});
 });
 
-//Respond to a Post Request on the root route (/), the apps home page
-app.post('/', function(req, res){
-  res.send('Got a POST Request')
-});
+app.get('/previous', function(req, res, next){
+  res.render('previous', {layout: false});
+})
 
-//Respond to a PUT request to the /user route
-app.put('/user', function(req, res){
-  res.send('Got a PUT request at /user')
-});
-
-//Respond to a DELETE request to the /user route
-app.delete('/user', function(req, res){
-  res.send('Got a DELETE request at /user')
-});
-
-
-var port = 3000;
+var port = process.env.CRUD_PORT_NR || 3000;
 app.listen(port, function(){
   console.log("Listening on port " + port);
 })
