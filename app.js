644c828582862fb492bdfac2749b8e9852b845dd
radlee <lolito@projectcodex.co>
2017-03-03 23:31:50 +0200

    Main Page Correctly Set Up and Styles improved

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
-// var mysql = require('mysql');
-var previous = require('./routes/previous');
+var mysql = require('mysql');
+var excercise = require('./routes/excercise');
 
 var app = express();
 
@@ -24,19 +24,22 @@ app.use(bodyParser.json()); //Support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); //Support encoded bodies
 
 //Configure ExpressJs to display content from static files
-app.use(express.static(__dirname + '/public'));
 
 //Configure Handlebars as a view engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(express.static(__dirname + '/public'));
 //Simple Routing 1.1
 app.get('/', function(req, res, next){
-  res.render('home', {layout: false});
+  res.render('home');
 });
 
 app.get('/previous', function(req, res, next){
-  res.render('previous', {layout: false});
+  res.render('previous');
+})
+app.get('/excercise/current', function(req, res, next){
+  res.render('current');
 })
 
 var port = process.env.CRUD_PORT_NR || 3000;
