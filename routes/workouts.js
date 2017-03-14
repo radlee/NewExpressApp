9e7b6ab72905328d540f99c9be917dd6a82fc3f4
radlee <lolito@projectcodex.co>
2017-03-14 12:36:27 +0200

    Just before Major Merge and Complete Project -- This commit you can revert back to

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,27 +1,25 @@
+//Define a function that will display the data from the 'workouts' table--
 exports.show = function (req, res, next) {
 	req.getConnection(function(err, connection){
 		if (err) return next(err);
+		//Connect to mysql database and SELECT data from 'workouts' table--
 		connection.query('SELECT * from workouts', [], function(err, results) {
         if (err) return next(err);
-		res.render( 'prev_workouts', {
-				no_workouts : results.length === 0,
-				workouts : results,
+				res.render( 'prev_workouts', {
+					no_workouts : results.length === 0,
+					workouts : results,
+				});
+			});
 		});
-      });
-	});
-};
-exports.showAdd = function(req, res){
-	res.render('add_workout');
-};
+	};
+	exports.showAdd = function(req, res){
+		res.render('add_workout');
+	};
 
-
-exports.add = function (req, res, next){
-	req.getConnection(function(err, connection){
+	exports.add = function (req, res, next){
+		req.getConnection(function(err, connection){
 			if(err) return next(err);
 			var input = req.body;
-			if(input.duration > 0){
-				console.log("Fix Moment----------------");
-			}
 
 			var data = {
 				the_date : input.the_date,
@@ -29,13 +27,26 @@ exports.add = function (req, res, next){
 				duration : input.duration
 			}
 
-		connection.query('INSERT INTO current set ?', data, function(err, results){
-			if(err) return next(err);
-			res.redirect('/current');
+			//if there is no input for workout redirect with a flash message
+
+			// if(data.workout === ''){
+			// 		res.redirect('/add_workout/add')
+			// 		next();
+			// }
+			// else {
+
+				//Continue to INSERT the Data
+				
+			// }
+
+			connection.query('INSERT INTO current set ?', data, function(err, results){
+				if(err) return next(err);
+				res.redirect('/current');
+			});
+
+			connection.query('INSERT INTO workouts set ?', data, function(err, results){
+				if(err) return next(err);
+				next();
+			})
 		})
-		connection.query('INSERT INTO workouts set ?', data, function(err, results){
-			if(err) return next(err);
-			next();
-		})
-	})
-}
+	}
