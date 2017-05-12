baeb18e7ccc0920f986d4171eb28dfd48e1cec5e
radlee <lolito@projectcodex.co>
2017-05-12 11:38:53 +0200

    Fist Commit of Heroku

--- app.js
+++ app.js
@@ -2,28 +2,10 @@
 var express = require('express');
 var cool = require('cool-ascii-faces');
 var exphbs = require('express-handlebars');
-var myConnection = require('express-myconnection')
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
-var workouts = require('./routes/workouts');
-var current = require('./routes/current');
-var moment = require('moment');
-var count = require('./public/js/countDays');
 
 //Start Express app
-
 var app = express();
-moment().format();
-
-//My SQL --
-var dbOptions ={
- host : 'localhost',
- user : 'root',
- password : 'password',
- port : 3306,
- database : 'my_db'
- };
-// END MYSQL --
 
 //Configure BodyParser to accept input from the browser--
 
@@ -38,7 +20,6 @@ app.set('view engine', 'handlebars');
 //Configure ExpressJs to display content from static files--
 
 app.use(express.static(__dirname + '/public'));
-app.use(myConnection(mysql, dbOptions, 'single'));
 
 //Define Error Handler to catch them and Display them on the browser--
 
@@ -46,28 +27,19 @@ function errorHandler(err, req, res, next){
     res.status(500);
     res.render('error', {error: err});
 }
-console.log("count----");
-console.log(count);
-console.log(workouts.show);
 
 //Simple Routing 1.1--
 
 app.get('/', function(req, res, next){
    res.render('home');
 });
-app.get('/events', function(req, res, next){
-    res.render('events');
+app.get('/blog', function(req, res, next){
+    res.render('blog');
 });
 
 //Another Level routing--
 
-app.get('/current',count.calculateDays);
-app.get('/prev_workouts', workouts.show);
-app.get('/add_workout/add', workouts.showAdd);
-app.post('/add_workout/add', workouts.add);
-app.get('/current', current.show);
-app.get('/current', current.show);
-app.get('/current/add', current.showAdd);
+
 
 app.use(errorHandler);
 
